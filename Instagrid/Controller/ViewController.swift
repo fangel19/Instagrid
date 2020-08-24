75ce3529c570be673c4b941ec6bc2d916e371ee0
fangel19 <fournyangel@msn.com>
2020-08-24 17:20:23 +0200

    Creation of variables for buttons, views with use of the Contral Drag

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -12,12 +12,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var swipeLabel: UILabel!
     
+    //Blue frame view for photo display
+    @IBOutlet weak var bluePhotoFrameView: UIView!
+    
+    //Outlet of each button to add photos
+    @IBOutlet weak var photoTopLeft: UIButton!
+    @IBOutlet weak var photoTopRight: UIButton!
+    @IBOutlet weak var photoDownLeft: UIButton!
+    @IBOutlet weak var photoDownRight: UIButton!
+    
+    //Outlet button style
+    @IBOutlet weak var buttonPhotoRectangleTop: UIButton!
+    @IBOutlet weak var buttonPhotoRectangleDown: UIButton!
+    @IBOutlet weak var buttonPhotoSquare: UIButton!
+    
+    //put a marker on the choice of photos positions
+    @IBOutlet weak var selectedPhotoView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         swipeLabel.text = "^\nSwipe up to share"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapButtonPhotoRectangleTop() {
+        startChooseNewButtonView()
+    }
+    private func startChooseNewButtonView() {
+        
+    }
 }
 
